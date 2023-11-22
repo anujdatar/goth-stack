7df7467c3484276381003b0e5db92f8fe4ea5469
Anuj Datar <anuj.datar@gmail.com>
2023-11-22 18:24:00 +0530

    simplest possible go/gin server with just a basic home route

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.New()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	fmt.Println("Server is running on port 8080")
+	fmt.Println("Connect using http://localhost:8080 or http://0.0.0.0:8080")
+
+	r.Run()
+}
