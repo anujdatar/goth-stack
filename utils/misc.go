3ec87da002079b8951fc84ff03c185dc868920c8
Anuj Datar <anuj.datar@gmail.com>
2023-12-05 13:13:06 +0530

    Added funcs to generate random strings and numbers(as strings for OTPs)

---
+++ utils/misc.go
@@ -0,0 +1,21 @@
+package utils
+
+import "math/rand"
+
+func GenerateRandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func GenerateRandomNumber(n int) string {
+	var letters = []rune("0123456789")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
